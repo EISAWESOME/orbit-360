9107006c177a15451c23539482dd70e2c3951df4
csimonin <c.simonin@lyon.ort.asso.fr>
2017-05-17 11:43:07 +0200

    premier commit

---
+++ app/scripts/app.js
@@ -0,0 +1,293 @@
+/*global angular, window, document, navigator, parseInt */
+'use strict';
+
+var ob = angular.module('Orbit', ['ngResource', 'hmGestures', 'mousewheel']);
+
+ob.controller('OrbitCtrl', ['$scope', '$rootScope', 'Images', function ($scope, $rootScope, Images) {
+    $scope.init = function () {
+        console.log('init');
+        $scope.canvas = document.getElementById('orbit-canvas');
+        $scope.renderer = $scope.canvas.getContext('2d');
+
+        $scope.level = Images.level.length-1;
+        Images.loadLevel($scope.level);
+
+        window.onresize = $scope.resize;
+        $scope.resize();
+
+        $scope.visible = true;
+        setInterval(function(){
+            $scope.draw();
+            // if($scope.waitingload){
+                $scope.loadingReso = $scope.waitingload;
+            // }
+        }, 40);
+
+        Images.loadLevel($scope.level);
+    };
+
+    $scope.loading = '0';
+    $scope.loadingReso = false;
+    $scope.visible = false;
+
+    $scope.zoom = 1;            //1 = zoom à 100%
+    $scope.level = 0;
+    $scope.angle = 0;         //id de l'angle de vue
+    $scope.edited = true;
+    $scope.waitingload = true;
+
+    $scope.fps = 0;
+    $scope.renderRatio = 5;
+    $scope.showTooltips = false;
+    $scope.autoPlay = true;
+    $scope.tooltips = [];
+    $scope.tooltip = null;
+    $scope.tooltipVisible = false;
+    $scope.goingFrom = null;
+
+    $rootScope.$on('onFirstComplete', function () {
+        // console.log('onFirstComplete');
+        $scope.edited = true;
+        $scope.setAngle($scope.angle + 1);
+    });
+    //A revoir
+    $rootScope.$on('onCurrentComplete', function (/*event, angle*/) {
+        // console.log('onCurrentComplete '+ angle);
+        // $scope.angle = angle;
+        // $scope.draw();
+    });
+    $rootScope.$on('onComplete', function () {
+        var time = new Date();
+        console.log('onComplete time: '+ time.getSeconds() +' '+ time.getMilliseconds());
+        $scope.loading = false;
+    });
+    $rootScope.$on('onLoading', function (event, percent) {
+        // console.log(percent);
+        $scope.loading = percent;
+    });
+
+    $scope.setAngle = function(angle){
+        if(angle >= Images.nbAngle) $scope.angle = angle - Images.nbAngle;
+        else if (angle < 0) $scope.angle = angle + Images.nbAngle;
+        else $scope.angle = angle;
+        $scope.edited = true;
+    };
+
+    $scope.selectTooltip = function (id) {
+        if ($scope.goingFrom !== null) {
+            return;
+        }
+        $scope.autoPlay = false;
+        $scope.tooltip = $scope.tooltips[id];
+        $scope.tooltip.id = id;
+        $scope.tooltipVisible = false;
+        $scope.goingFrom = $scope.angle;
+        $scope.goTo();
+    };
+    $scope.getTooltipX = function () {
+        if ($scope.tooltip === null) {
+            return 0;
+        } else {
+            return ($scope.tooltip.x * $scope.zoom) + $scope.getX() - 1;
+        }
+    };
+    $scope.getTooltipY = function () {
+        if ($scope.tooltip === null) {
+            return 0;
+        } else {
+            return ($scope.tooltip.y * $scope.zoom) + $scope.getY() + 5;
+        }
+    };
+
+    $scope.goTo = function () {
+        console.log('goTo');
+        var t = $scope.iteration,
+            b = $scope.goingFrom,
+            c = $scope.tooltip.image - b,
+            d = Math.abs(Math.round(c / 2));
+        d = (d === 0) ? 1 : d;
+        t /= d;
+        t--;
+        //$scope.draw(Math.round(c * (t * t * t + 1) + b));
+        if ($scope.iteration > d) {
+            $scope.goingFrom = null;
+            $scope.tooltipVisible = true;
+            $scope.$apply();
+        }
+    };
+
+
+    $scope.resize = function () {
+        console.log('resize');
+        $scope.canvas.width = $scope.canvas.offsetWidth;
+        $scope.canvas.height = $scope.canvas.offsetHeight;
+        if ($scope.loading === true) {
+            return false;
+        }
+        if ($scope.canvas.width / Images.level[0].width <= $scope.canvas.height / Images.level[0].height) {
+            $scope.zoom = $scope.canvas.width / Images.level[0].width;
+        } else {
+            $scope.zoom = $scope.canvas.height / Images.level[0].height;
+        }
+        $scope.level = Images.level.length-1;
+        while($scope.zoom*1000 > Images.level[$scope.level].value){
+            $scope.level--;
+        }
+        $scope.minZoom = $scope.zoom;
+        $scope.maxZoom = 1;
+        if ($scope.visible) {
+            $scope.$apply();
+        }
+        $scope.edited = true;
+    };
+
+    $scope.play = function () {
+        var n = new Date();
+        // console.log('play '+ n.getMilliseconds());
+        if ($scope.autoPlay) {
+            if ($scope.tooltipVisible === true) {
+                $scope.tooltipVisible = false;
+                $scope.$apply();
+            }
+            //$scope.draw($scope.angle + 1);
+            $scope.setAngle($scope.angle + 1);
+            window.setTimeout($scope.play, 40);
+        }
+        if ($scope.goingFrom != null) {
+            $scope.goTo();
+            $scope.iteration++;
+        } else {
+            $scope.iteration = 0;
+        }
+    };
+
+    $scope.onWheel = function (e) {
+        var zoom = $scope.zoom;
+        if (e.deltaY > 0) {
+            $scope.zoomOut();
+        } else {
+            $scope.zoomIn();
+        }
+
+        if(zoom != $scope.zoom) $scope.edited = true;
+        //$scope.draw();
+    };
+    $scope.zoomOut = function () {
+        $scope.zoom -= 0.1;
+        if ($scope.zoom < $scope.minZoom) {
+            $scope.zoom = $scope.minZoom;
+        }
+        if($scope.level < Images.level.length-1 && $scope.zoom*1000 <= Images.level[$scope.level+1].value)
+            $scope.level++;
+    };
+    $scope.zoomIn = function () {
+        $scope.zoom += 0.1;
+        if ($scope.zoom >= $scope.maxZoom) {
+            $scope.zoom = $scope.maxZoom;
+        }
+        if($scope.level > 0 && $scope.zoom*1000 > Images.level[$scope.level].value)
+            $scope.level--;
+        //console.log($scope.zoom);
+    };
+
+
+    $scope.keymove = function (e) {
+        console.log(e.keyCode);
+        if(e.keyCode === 39)
+            $scope.setAngle($scope.angle -1);
+        if(e.keyCode === 37)
+            $scope.setAngle($scope.angle +1);
+    };
+
+    $scope.drag = function (e) {
+        console.log('drag');
+        $scope.tooltipVisible = false;
+        var dst = $scope.lastDrag - e.gesture.deltaX,
+            ratio;
+        dst *= 1;
+        ratio = dst / 10;
+        ratio = ratio.toFixed(0);
+        if (ratio === '-0') {
+            ratio = -1;
+        } else if (ratio === '0') {
+            ratio = 1;
+        }
+        $scope.lastDrag = e.gesture.deltaX;
+
+        //$scope.draw($scope.angle + parseInt(ratio));
+        $scope.setAngle($scope.angle + parseInt(ratio));
+    };
+
+    $scope.draw = function () {
+        // console.log('draw '+$scope.angle);
+
+        if(($scope.waitingload && Images.resourcesLoaded($scope.level, $scope.angle)) || $scope.edited){
+            $scope.waitingload = false;
+            // console.log('draw '+ lvl +' '+ $scope.angle +' zoom: '+ $scope.zoom);
+            $scope.renderer.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
+
+            var lvl = $scope.level;
+            var current = Images.level[lvl].resources[$scope.angle];
+            var pos = 0;
+            if(!Images.resourcesLoaded(lvl, $scope.angle)){
+                $scope.waitingload = true;
+                Images.loadResources(lvl, $scope.angle);
+
+                while(lvl < Images.level.length-1 && !Images.resourcesLoaded(lvl, $scope.angle)){
+                    lvl++;
+                }
+                current = Images.level[lvl].resources[$scope.angle];
+                // console.log('draw lvl: '+ lvl);
+            }
+            var ILvl = Images.level[lvl],
+                posOriX = $scope.getX(),
+                posOriY = $scope.getY(),
+                lapX = Math.floor(Images.level[0].width/ILvl.cols) * $scope.zoom,
+                lapY = Math.floor(Images.level[0].height/ILvl.rows) * $scope.zoom;
+
+            for (var i = 0; i < current.length; i++) {
+                // console.log(Images.loadImage(lvl, $scope.angle, i));
+                var posX = posOriX + lapX * Math.floor(i / ILvl.rows),
+                    posY = posOriY + lapY * Math.floor(i % ILvl.rows);
+
+                if (
+                    (-posOriX < posX + Math.floor(Images.level[0].width/ILvl.cols) &&
+                    -posOriY < posY + Math.floor(Images.level[0].height/ILvl.rows)) ||
+                    ($scope.canvas.width > posX &&
+                    $scope.canvas.height > posY )
+                ){
+                    $scope.renderer.drawImage(
+                        current[i].img,
+                        0,
+                        0,
+                        current[i].img.naturalWidth,
+                        current[i].img.naturalHeight,
+                        posX,
+                        posY,
+                        current[i].img.naturalWidth * $scope.zoom * 1000/ILvl.value,
+                        current[i].img.naturalHeight * $scope.zoom * 1000/ILvl.value
+                    );
+                }
+            }
+
+            $scope.edited = false;
+        }
+    };
+
+    $scope.getX = function () {
+        return +(($scope.canvas.width / 2) - ((Images.level[0].width / 2) * $scope.zoom)).toFixed(0);
+    };
+
+    $scope.getY = function () {
+        return -(((Images.level[0].height * $scope.zoom) - $scope.canvas.height) / 2).toFixed(0);
+    };
+
+    $scope.getWidth = function () {
+        return $scope.zoom * (Images.level[0].width);
+    };
+
+    $scope.getHeight = function () {
+        return $scope.zoom * (Images.level[0].height);
+    };
+
+}]);
---
+++ app/scripts/services.js
@@ -0,0 +1,166 @@
+/*global ob */
+'use strict';
+ob.factory('Images', ['$resource', '$rootScope', '$http', function ($resource, $rootScope, $http) {
+    return {
+        //url: '../hyracotherium_pied-a-4-doigts/',
+        url: '../Axinite_prenite_epidote/',
+        loadingQueue: [],
+        loadSlot: 0,
+        loaded: 0,
+        firstLevelLoaded: 0,
+        //charge un xml, contenu recuperable avec .success
+        loadxml: function(){
+            return $http.get(this.url +'content.xml');
+        },
+        loadLevel: function (lvl) {
+            var time = new Date();
+            console.log('loadLevel  '+ lvl +' time: '+ time.getSeconds() +' '+ time.getMilliseconds());
+
+            var scope = this.level[lvl];
+            var f = false;
+            for (var i = 0; i < scope.resources.length; i++) {
+                this.loadResources(lvl, i, f);
+            }
+        },
+        //loadResources avec queue, sans et avec slot
+        loadResources: function (lvl, angle, priority) {
+            priority = (typeof priority !== "undefined")? priority : true;
+            // console.log('prio '+ priority);
+            if(priority){
+                for (var i = 0; i < this.level[lvl].resources[angle].length; i++)
+                    this.loadingQueue.unshift([lvl, angle, i]);
+            }
+            else{
+                for (var i = 0; i < this.level[lvl].resources[angle].length; i++)
+                    this.loadingQueue.push([lvl, angle, i]);
+            }
+            this.loadQueuedImages();
+        },
+        //renvoie vrai si toutes les images inclus dans resources sont chargées
+        resourcesLoaded: function (lvl, agl) {
+            var angle = this.level[lvl].resources[agl];
+            for (var i = 0; i < angle.length; i++) {
+                if(!angle[i].loaded) return false;
+            }
+            return true;
+        },
+        //load sans queue
+        loadImage: function (lvl, angle, pos, fromQueue) {
+            fromQueue = fromQueue || false;
+            var self = this.level[lvl].resources[angle][pos];
+            var source = this.level[lvl].resources[angle][pos].img;//necessaire car cette info se perd si loadImage est executé plusieurs fois en parallèle
+            // console.log('type: '+ (typeof source !== 'string'));
+            if(typeof source !== 'string'){
+                if(fromQueue) this.loadQueuedImages();
+                return true;
+            }
+            else{
+                var scope = this;
+                var img = new Image();
+
+                img.src = source;
+                console.log(img);
+                scope.level[lvl].resources[angle][pos].img = img;
+                scope.loadSlot++;
+                $http.get(source, {method: 'GET'}).then(function() {
+                    // setTimeout(function(){
+                        console.log(img);
+                        self.loaded = true;
+                    // }, 1000);
+                    // console.log(scope.level[lvl].resources[angle][pos].loaded);
+                    scope.loadSlot--;
+                    if(fromQueue){
+                        // console.log('done '+ angle);
+                        scope.loadQueuedImages();
+                    }
+                    if(scope.firstLevelLoaded < scope.nbAngle && lvl == scope.level.length-1){
+                        scope.firstLevelLoaded++;
+                        // $rootScope.$emit('onFirstComplete');
+                        scope.loading(scope.firstLevelLoaded, scope.nbAngle);
+                    }
+                });
+                return false;
+            }
+        },
+        //load avec queue à plusieur slots
+        loadQueuedImages: function () {
+            if(this.loadSlot < 3 && this.loadingQueue.length > 0){
+                var current = this.loadingQueue.shift();
+                if(this.loadImage(current[0], current[1], current[2], true)){
+                    // console.log('deja chargé ' + current[0] +' '+ current[1] +' '+ current[2]);
+                }
+                // else console.log('chargement ' + current[0] +' '+ current[1] +' '+ current[2]);
+            }
+        },
+        //loading queue avec slot et sans slot
+        loading: function (current, max) {
+            // this.loaded += 1;
+            var percent = current*100 / max;
+            percent = percent.toFixed(1);
+
+                //A analyser pour comprendre pourquoi l'image n'est pas directement disponible au déclanchement de 'onFirstComplete'
+            // if (this.loaded == 1) /*setTimeout(function(){*/$rootScope.$emit('onFirstComplete');/*}, 1000);*/
+            if (percent >= 100) {
+                $rootScope.$emit('onComplete');
+            } else {
+                $rootScope.$emit('onLoading', percent);
+            }
+        }
+        //loadResources avec queue sans slot
+/*        loadResources: function (lvl, angle) {
+            for (var i = 0; i < this.level[lvl].resources[angle].length; i++) {
+                this.loadingQueue.push([lvl, angle, i]);
+            }
+            if(this.loaded != 0){
+                this.loaded = 0;
+                this.loadImage();
+            }
+
+            // console.log(angle);
+        },*/
+        //load avec queue sans slot
+/*        loadImage: function () {
+            // console.log('loadImage');
+            // $rootScope.$emit('onLoading', percent);
+            var scope = this;
+            var queue = scope.loadingQueue.length;
+            console.log('loadImage '+ queue );
+            // if(scope.loadSlot < 10 && scope.loadingQueue.length > 0){
+            //     scope.loadSlot++;
+            var current = scope.loadingQueue.shift();
+            var img = new Image();
+            var source = this.level[current[0]].resources[current[1]][current[2]];//necessaire car cette info se perd si loadImage est executé plusieurs fois en parallèle
+            // console.log(source);
+            $resource(source).get(function () {
+                // console.log('$resource.get: ' + scope.level[current[0]].resources[current[1]][current[2]]);
+                // console.log(source);
+
+                // scope.loadSlot -= 1;
+                img.src = source;
+                img.isLoaded = true;
+                scope.level[current[0]].resources[current[1]][current[2]] = img;
+                // console.log(angle);
+                $rootScope.$emit('onCurrentComplete', current[1]);
+                scope.loading();
+                if(scope.loadingQueue.length == 0) scope.loaded = 1;
+                if(scope.loaded == 0){
+                    scope.loadImage();
+                }
+            });
+            // }
+        },*/
+        //loading queue sans slot
+/*        loading: function (lvl) {
+            // this.loaded += 1;
+            // var percent = this.loaded * 100 / this.level[0].resources.length;
+            var percent = this.loaded * 100;
+            percent = percent.toFixed(1);
+            if (this.loaded == 1) $rootScope.$emit('onFirstComplete');
+            if (this.loaded !== this.level[lvl].resources.length) {
+                $rootScope.$emit('onLoading', percent);
+            } else {
+                $rootScope.$emit('onComplete');
+            }
+        }*/
+    };
+}]);
