06083f33d14197a86493a6d33be5a99b9158ecb7
Clément S <nkz_@hotmail.fr>
2018-03-11 17:53:03 +0100

    gulpfile to build + updated dependencies

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const gulp = require('gulp');
+
+const less = require('gulp-less');
+const minifyCSS = require('gulp-csso');
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+const rename = require('gulp-rename');
+
+const gulpUtil = require('gulp-util');
+
+gulp.task('index', function () {
+  return gulp.src('index.build.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('../build/'));
+});
+
+gulp.task('root', function () {
+  return gulp.src([
+    'sw.js',
+    'orbit360.webmanifest',
+    'robots.txt'
+  ])
+    .pipe(gulp.dest('../build/'));
+});
+
+gulp.task('views', function () {
+  return gulp.src('views/*.html')
+    .pipe(gulp.dest('../build/views'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('styles/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('../build/styles'));
+});
+
+gulp.task('lib_css', function () {
+  return gulp.src('node_modules/angular-material/angular-material.min.css')
+    .pipe(gulp.dest('../build/styles'));
+});
+
+gulp.task('css', ['less', 'lib_css']);
+
+gulp.task('js', function () {
+  return gulp.src('scripts/**/*.js')
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglify().on('error', gulpUtil.log))
+    .pipe(gulp.dest('../build/'));
+});
+
+gulp.task('resources', function () {
+  return gulp.src('resources/**/*')
+    .pipe(gulp.dest('../build/resources'));
+});
+
+gulp.task('lib', function () {
+  return gulp.src('lib/*.js')
+    .pipe(gulp.dest('../build/lib'));
+});
+
+gulp.task('node_modules', function () {
+  return gulp.src([
+    'node_modules/fg-loadcss/dist/cssrelpreload.min.js',
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-messages/angular-messages.min.js',
+    'node_modules/angular-resource/angular-resource.min.js',
+    'node_modules/angular-material/angular-material.min.js',
+    'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
+    'node_modules/jszip/dist/jszip.min.js',
+    'node_modules/file-saver/FileSaver.min.js',
+    'node_modules/angular-aria/angular-aria.min.js'
+  ])
+    .pipe(gulp.dest('../build/lib'));
+});
+
+gulp.task('fa', function () {
+  return gulp.src('node_modules/font-awesome/**/*')
+    .pipe(gulp.dest('../build/lib/font-awesome'));
+});
+
+gulp.task('libs', ['lib', 'node_modules', 'fa']);
+
+gulp.task('build', [ 'index', 'root', 'views', 'css', 'js', 'resources', 'libs' ]);
